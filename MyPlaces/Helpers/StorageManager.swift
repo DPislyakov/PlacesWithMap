c8504954a276fccd32dcebe2444510055d2ca6af
dmitrijpislakov <dpislyakov@bk.ru>
2022-05-19 23:35:01 +0200

    Final prepare

---
+++ MyPlaces/Helpers/StorageManager.swift
@@ -0,0 +1,26 @@
+//
+//  StorageManager.swift
+//  MyPlaces
+//
+//  Created by Дмитрий Писляков on 13.05.2022.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+    
+    static func saveObject(_ place: Place) {
+        
+        try! realm.write() {
+            realm.add(place)
+        }
+    }
+    
+    static func deleteObject(_ place:Place) {
+        try! realm.write(){
+            realm.delete(place)
+        }
+    }
+}
---
+++ MyPlaces/Models/PlaceModel.swift
@@ -0,0 +1,30 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Дмитрий Писляков on 28.04.2022.
+//
+
+import RealmSwift
+import UIKit
+import Foundation
+
+class Place: Object {
+    
+    @objc dynamic var name = ""
+    @objc dynamic var location: String?
+    @objc dynamic var type: String?
+    @objc dynamic var imageData: Data?
+    @objc dynamic var date = Date()
+    @objc dynamic var rating = 0.0
+    
+    convenience init(name: String, location: String?, type: String?, imageData: Data?, rating: Double) {
+        self.init()
+        self.name = name
+        self.location = location
+        self.type = type
+        self.imageData = imageData
+        self.rating = rating
+    }
+        
+}
